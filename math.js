cd07c55ffc07c9e0df0176a2f1baf207c5bec0fe
mumuy <zhenghaole1990@126.com>
2016-06-27 18:05:35 +0800

    updated 进制转换方法

--- math.js
+++ math.js
@@ -28,3 +28,14 @@ function simple(a,b){
     })(a,b);
     console.log('最小整数比：'+(a/c)+':'+(b/c));
 }
+
+//将10进制转成其他进制（parseInt只支持2-36）
+function parseFromDecimal(scale,decimalNumber){
+    var scale64 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!";
+    var result = '';
+    do{
+        result = scale64.charAt(decimalNumber % scale) + result;
+        decimalNumber = Math.floor(decimalNumber/scale);
+    }while(decimalNumber);
+    return result;
+}
